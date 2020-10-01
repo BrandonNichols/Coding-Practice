6e95a891f706f518f50d51f2db267968450f3bb6
Brandon Nichols <brandonaronnichols@gmail.com>
2020-09-30 22:58:47 -0500

    working on longestUnivaluePath current solution not working

--- recurion_practice.py
+++ recurion_practice.py
@@ -14,3 +14,43 @@ def sum(arr, n):
 print(sum([1], 0)) #return 0
 print(sum([2, 3, 4], 1)) #return 2
 print(sum([2, 3, 4, 5], 3)) #return 9
+
+'''
+Longest Univalue Path
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+        
+    def longestUnivaluePath(self, root):
+        self.current_count = 0
+        self.current_value = 0
+        self.highest_count = 0
+        
+        def longPath(root):
+            
+            if root is None:
+                return  
+            
+            if self.current_value == root.val:
+                self.current_count += 1
+                
+                if self.highest_count < self.current_count:
+                    self.highest_count = self.current_count
+                    
+            self.current_value = root.val
+            longPath(root.left)
+            longPath(root.right)
+            
+            return
+        
+        longPath(root)
+        
+        return self.highest_count
